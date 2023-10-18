2c5e1c1755d23780ebf28e8c9c6e745df54193d7
vzboun <129953847+vzboun@users.noreply.github.com>
2023-10-18 12:36:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function tocaSom (idElementoAudio) {
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //para
-for (let contador = 0; contador < listaDeTeclas.length; contador+++) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas [contador];
     const instrumento = tecla.classList[1];
@@ -17,9 +17,11 @@ for (let contador = 0; contador < listaDeTeclas.length; contador+++) {
     
     tecla.onkeydown = function (evento) {
 
-        console.log(evento.code)
+        if (evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
 
-        if (evento.code = 'Space') {
+        if (evento.code === 'Enter') {
                tecla.classList.add('ativa');
             
         }
